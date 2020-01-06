36dc9099f17df73332608cdc01a5af899978e6fd
zkutrol <roland.kutas@blackbelt.hu>
2020-01-06 15:33:49 +0100

    Modularized the project

--- backend/src/test/java/micronaut/demo/PersonControllerTest.java
+++ backend/src/test/java/micronaut/demo/PersonControllerTest.java
@@ -6,30 +6,30 @@ import static org.hamcrest.Matchers.hasSize;
 import io.micronaut.runtime.server.EmbeddedServer;
 import io.micronaut.test.annotation.MicronautTest;
 import javax.inject.Inject;
-import micronaut.demo.client.PersonClient;
+import micronaut.demo.controller.PersonController;
 import micronaut.demo.model.Person;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 @MicronautTest
-public class PersonControllerTest {
+class PersonControllerTest {
 
     @Inject
     EmbeddedServer embeddedServer;
 
     @Inject
-    PersonClient personClient;
+    PersonController personController;
 
     @Test
-    public void testGetAllPersons() {
+    void testGetAllPersons() {
         Person person = new Person();
         person.setId(1);
         person.setName("Test Person");
         person.setSalary(10000);
 
-        personClient.addPerson(person);
+        personController.addPerson(person);
 
-        assertThat(personClient.getPersons(), hasSize(1));
-        assertThat(personClient.getPersons(), Matchers.hasItem(person));
+        assertThat(personController.getPersons(), hasSize(1));
+        assertThat(personController.getPersons(), Matchers.hasItem(person));
     }
 }
---
+++ client/src/main/java/micronaut/demo/model/Person.java
@@ -0,0 +1,51 @@
+package micronaut.demo.model;
+
+import java.util.Objects;
+
+public class Person {
+
+    private int id;
+    private String name;
+    private double salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
